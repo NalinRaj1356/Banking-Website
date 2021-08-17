1a92f9606ac78a36797ff2619dfbc69bf6165b29
NalinRaj1356 <77092278+NalinRaj1356@users.noreply.github.com>
2021-08-17 17:22:25 +0530

    Add files via upload

---
+++ Banking_website-main/Credit/company.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 03, 2020 at 06:59 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `company`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(15) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `credit` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `email`, `credit`) VALUES
+('akshay', 'aks@gmail.com', '110345'),
+('Anirud', 'ani@gmail.com', '36000'),
+('Ganesh', 'ganesh@gmail.com', '39500'),
+('manoj', 'manoj@gmail.com', '10000'),
+('nikhil', 'niki@gmail.com', '123345'),
+('Ryan', 'Ryan@gmail.com', '989226'),
+('sai', 'sai@gmail.com', '9718'),
+('Shreyas', 'Shreyas@gmail.com', '34067'),
+('Tejas', 'tejas@gmail.com', '1234'),
+('vaibhav', 'vaibav@gmail.com', '200000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Banking_website-main/README.md
@@ -0,0 +1,4 @@
+# banking
+Spark Foundation Internship-GRIP<br/>
+The preview of my webpage<br/>
+![Capture](https://user-images.githubusercontent.com/49776470/98074980-7e1f3e00-1e91-11eb-8af8-f4913faf1208.PNG)
